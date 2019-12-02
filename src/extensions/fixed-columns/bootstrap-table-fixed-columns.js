67511032229cb9281c27ee143317123b582513fc
michael <michael@mictrl.com>
2019-12-02 21:49:04 +0800

    定制固定列

--- src/extensions/fixed-columns/bootstrap-table-fixed-columns.js
+++ src/extensions/fixed-columns/bootstrap-table-fixed-columns.js
@@ -14,11 +14,10 @@ $.extend($.fn.bootstrapTable.defaults, {
 })
 
 $.BootstrapTable = class extends $.BootstrapTable {
-
-  fitHeader (...args) {
+  fitHeader(...args) {
     super.fitHeader(...args)
 
-    console.log('fitHeader')
+    // console.log('fitHeader')
     if (!this.options.leftFixedColumns && !this.options.rightFixedColumns) {
       return
     }
@@ -39,10 +38,10 @@ $.BootstrapTable = class extends $.BootstrapTable {
     this.initFixedColumnsBody()
   }
 
-  initBody (...args) {
+  initBody(...args) {
     super.initBody(...args)
 
-    console.log('initBody', this.options, !this.options.leftFixedColumns && !this.options.rightFixedColumns)
+    // console.log('initBody', this.options, !this.options.leftFixedColumns && !this.options.rightFixedColumns)
     if (!this.options.leftFixedColumns && !this.options.rightFixedColumns) {
       return
     }
@@ -50,103 +49,78 @@ $.BootstrapTable = class extends $.BootstrapTable {
     if (this.options.showHeader && this.options.height) {
       return
     }
-
-
   }
 
-  initFixedColumns () {
-    console.log('initFixedColumns', this)
+  initFixedColumns() {
+    // console.log('initFixedColumns', this)
     this.timeoutHeaderColumns_ = 0
     this.timeoutBodyColumns_ = 0
     this.$container.find('.fixed-table-header-columns').remove()
     this.$container.find('.fixed-table-body-columns').remove()
     if (this.options.leftFixedColumns) {
       // var leftWidth = this.getLeftFixedColumnsWidth()
-      this.$leftFixedHeader = $([
-        '<div class="fixed-table-header-columns fixed-table-header-columns-left">',
-        '<table>',
-        '<thead></thead>',
-        '</table>',
-        '</div>'].join(''))
+      this.$leftFixedHeader = $(
+        [
+          '<div class="fixed-table-header-columns fixed-table-header-columns-left">',
+          '<table>',
+          '<thead></thead>',
+          '</table>',
+          '</div>'
+        ].join('')
+      )
       this.$leftFixedHeader.find('table').attr('class', this.$el.attr('class'))
       this.$leftFixedHeaderColumns = this.$leftFixedHeader.find('thead')
 
       this.$tableHeader.after(this.$leftFixedHeader)
 
-
-      // const leftWidth = this.getLeftFixedColumnsWidth()
-
-      // this.$leftFixedHeader.css({
-      //   top: 0,
-      //   width: leftWidth,
-      //   height: this.$tableHeader.outerHeight(true)
-      // })
-
-
-      this.$leftFixedBody = $([
-        '<div class="fixed-table-body-columns fixed-table-body-columns-left">',
-        '<table>',
-        '<tbody></tbody>',
-        '</table>',
-        '</div>'].join(''))
+      this.$leftFixedBody = $(
+        [
+          '<div class="fixed-table-body-columns fixed-table-body-columns-left">',
+          '<table>',
+          '<tbody></tbody>',
+          '</table>',
+          '</div>'
+        ].join('')
+      )
 
       this.$leftFixedBody.find('table').attr('class', this.$el.attr('class'))
       this.$leftFixedBodyColumns = this.$leftFixedBody.find('tbody')
       this.$tableBody.after(this.$leftFixedBody)
-
-      // this.$leftFixedBody.css({
-      //   top: this.$tableHeader.outerHeight(true),
-      //   width: leftWidth,
-      //   height: this.$tableBody.outerHeight(true) - 1
-      // })
     }
     if (this.options.rightFixedColumns) {
       // var rightWidth = this.getRightFixedColumnsWidth()
-      this.$rightFixedHeader = $([
-        '<div class="fixed-table-header-columns fixed-table-header-columns-right" style="z-index:100;background:#fff;">',
-        '<table>',
-        '<thead></thead>',
-        '</table>',
-        '</div>'].join(''))
+      this.$rightFixedHeader = $(
+        [
+          '<div class="fixed-table-header-columns fixed-table-header-columns-right" style="z-index:100;background:#fff;">',
+          '<table>',
+          '<thead></thead>',
+          '</table>',
+          '</div>'
+        ].join('')
+      )
 
       this.$rightFixedHeader.find('table').attr('class', this.$el.attr('class'))
       this.$rightFixedHeaderColumns = this.$rightFixedHeader.find('thead')
       this.$tableHeader.after(this.$rightFixedHeader)
 
-      // const rightWidth = this.getRightFixedColumnsWidth()
-
-      // this.$rightFixedHeader.css({
-      //   top: 0,
-      //   right: '14px',
-      //   width: rightWidth,
-      //   height: this.$tableHeader.outerHeight(true)
-      // })
-
-
-      this.$rightFixedBody = $([
-        '<div class="fixed-table-body-columns fixed-table-body-columns-right" style="z-index:100;background:#fff;">',
-        '<table>',
-        '<tbody></tbody>',
-        '</table>',
-        '</div>'].join(''))
+      this.$rightFixedBody = $(
+        [
+          '<div class="fixed-table-body-columns fixed-table-body-columns-right" style="z-index:100;background:#fff;">',
+          '<table>',
+          '<tbody></tbody>',
+          '</table>',
+          '</div>'
+        ].join('')
+      )
 
       this.$rightFixedBody.find('table').attr('class', this.$el.attr('class'))
       this.$rightFixedBodyColumns = this.$rightFixedBody.find('tbody')
       this.$tableBody.after(this.$rightFixedBody)
-
-      // this.$rightFixedBody.css({
-      //   top: this.$tableHeader.outerHeight(true),
-      //   right: '14px',
-      //   width: rightWidth,
-      //   height: this.$tableBody.outerHeight(true) - 1
-      // })
-
     }
   }
 
-
-  initFixedColumnsHeader () {
-    console.log('initFixedColumnsHeader', this.$header, this.$tableHeader)
+  initFixedColumnsHeader() {
+    // console.log('initFixedColumnsHeader', this.$header, this.$tableHeader)
     var $tr = this.$header.find('tr:eq(0)').clone()
     var $ths = $tr.clone().find('th')
 
@@ -162,6 +136,7 @@ $.BootstrapTable = class extends $.BootstrapTable {
         this.$leftFixedWidth += $th.width()
         $newtr.append($th)
       }
+      // this.$leftFixedWidth += 1
       this.$leftFixedHeaderColumns.html('').append($newtr)
     }
     // $tr.html('');
@@ -177,12 +152,11 @@ $.BootstrapTable = class extends $.BootstrapTable {
       }
       this.$rightFixedHeaderColumns.html('').append($newtr2)
     }
-
   }
 
-  initFixedColumnsBody () {
+  initFixedColumnsBody() {
     const that = this
-    console.log('initFixedColumnsBody')
+    // console.log('initFixedColumnsBody')
 
     this.$table = this.$tableBody.find('>table')
 
@@ -223,103 +197,45 @@ $.BootstrapTable = class extends $.BootstrapTable {
         that.$rightFixedBodyColumns.append($tr)
       })
     }
-
-
-
-    // that.$tableHeader.find('thead th.bs-checkbox').on('change', 'input[type="checkbox"]', function (e) {
-    //   console.log('tableHeader', $(this).prop('checked'))
-    // })
-
-    // that.$tableBody.find('tbody td.bs-checkbox').on('change', 'input[type="checkbox"]', function (e) {
-    //   console.log('tableBody', $(this).prop('checked'))
-    // })
   }
 
-  // getLeftFixedColumnsWidth() {
-  //   console.log('getLeftFixedColumnsWidth')
-  //   const visibleFields = this.getVisibleFields()
-  //   let width = 0
-
-  //   for (let i = 0; i < this.options.leftFixedNumber; i++) {
-  //     width += this.$header.find(`th[data-field="${visibleFields[i]}"]`).outerWidth(true)
-  //   }
-
-  //   return width + 1
-  // }
-
-  // getRightFixedColumnsWidth() {
-  //   const visibleFields = this.getVisibleFields()
-  //   let width = 0
-  //   const visibleFieldsLength = visibleFields.length - 1
-  //   for (let i = 0; i < this.options.rightFixedNumber; i++) {
-
-  //     width += this.$header.find(`th[data-field="${visibleFields[visibleFieldsLength - i]}"]`).outerWidth(true)
-  //     console.log('getRightFixedColumnsWidth', visibleFieldsLength - i, visibleFields[visibleFieldsLength - i], width)
-  //   }
-
-  //   return width + 1
-  // }
-
-
-  // initFixedColumnsEvents($el) {
-  //   // events
-  //   this.$tableBody.on('scroll.fixed-columns', e => {
-  //     $el.find('table').css('top', -$(e.currentTarget).scrollTop())
-  //   })
-
-  //   this.$body.find('> tr[data-index]').off('hover').hover(e => {
-  //     const index = $(e.currentTarget).data('index')
-  //     $el.find(`tr[data-index="${index}"]`)
-  //       .css('background-color', $(e.currentTarget).css('background-color'))
-  //   }, e => {
-  //     const index = $(e.currentTarget).data('index')
-  //     const $tr = $el.find(`tr[data-index="${index}"]`)
-  //     $tr.attr('style', $tr.attr('style').replace(/background-color:.*;/, ''))
-  //   })
-
-  //   $el.find('tr[data-index]').off('hover').hover(e => {
-  //     const index = $(e.currentTarget).data('index')
-  //     this.$body.find(`tr[data-index="${index}"]`)
-  //       .css('background-color', $(e.currentTarget).css('background-color'))
-  //   }, e => {
-  //     const index = $(e.currentTarget).data('index')
-  //     const $tr = this.$body.find(`> tr[data-index="${index}"]`)
-  //     $tr.attr('style', $tr.attr('style').replace(/background-color:.*;/, ''))
-  //   })
-  // }
-
-
-  resetView (...args) {
+  resetView(...args) {
     super.resetView(...args)
-
 
     if (!this.options.leftFixedColumns && !this.options.rightFixedColumns) {
       return
     }
 
     clearTimeout(this.timeoutHeaderColumns_)
-    this.timeoutHeaderColumns_ = setTimeout($.proxy(this.resetViewHeaderColumns, this), this.$el.is(':hidden') ? 100 : 0)
+    this.timeoutHeaderColumns_ = setTimeout(
+      $.proxy(this.resetViewHeaderColumns, this),
+      this.$el.is(':hidden') ? 100 : 0
+    )
 
     clearTimeout(this.timeoutBodyColumns_)
     this.timeoutBodyColumns_ = setTimeout($.proxy(this.resetViewBodyColumns, this), this.$el.is(':hidden') ? 100 : 0)
   }
 
-  resetViewHeaderColumns () {
+  resetViewHeaderColumns() {
     var that = this
 
     if (that.options.leftFixedColumns) {
-      this.$leftFixedHeader.css({
-        width: this.$leftFixedWidth
-      }).show()
+      this.$leftFixedHeader
+        .css({
+          width: this.$leftFixedWidth
+        })
+        .show()
     }
     if (that.options.rightFixedColumns) {
-      this.$rightFixedHeader.css({
-        width: this.$rightFixedWidth + 16
-      }).show()
+      this.$rightFixedHeader
+        .css({
+          width: this.$rightFixedWidth
+        })
+        .show()
     }
   }
 
-  resetViewBodyColumns () {
+  resetViewBodyColumns() {
     var that = this
     var top = this.$tableHeader.height()
     var height = this.$tableBody.height()
@@ -334,12 +250,13 @@ $.BootstrapTable = class extends $.BootstrapTable {
         height -= top
       }
 
-      this.$leftFixedBody.css({
-        width: this.$leftFixedWidth,
-        height: height - 13,
-        top: top
-      }).show()
-
+      this.$leftFixedBody
+        .css({
+          width: this.$leftFixedWidth,
+          height: height - 14,
+          top: top
+        })
+        .show()
 
       this.$body.find('> tr').each(function (i) {
         that.$leftFixedBody.find('tbody tr:eq(' + i + ')').height($(this).height())
@@ -349,20 +266,32 @@ $.BootstrapTable = class extends $.BootstrapTable {
       this.$tableBody.on('scroll', function () {
         that.$leftFixedBody.find('table').css('top', -$(this).scrollTop())
       })
-      this.$body.find('> tr[data-index]').off('hover').hover(function () {
-        var index = $(this).data('index')
-        that.$leftFixedBody.find('tr[data-index="' + index + '"]').addClass('hover')
-      }, function () {
-        var index = $(this).data('index')
-        that.$leftFixedBody.find('tr[data-index="' + index + '"]').removeClass('hover')
-      })
-      this.$leftFixedBody.find('tr[data-index]').off('hover').hover(function () {
-        var index = $(this).data('index')
-        that.$body.find('tr[data-index="' + index + '"]').addClass('hover')
-      }, function () {
-        var index = $(this).data('index')
-        that.$body.find('> tr[data-index="' + index + '"]').removeClass('hover')
-      })
+      this.$body
+        .find('> tr[data-index]')
+        .off('hover')
+        .hover(
+          function () {
+            var index = $(this).data('index')
+            that.$leftFixedBody.find('tr[data-index="' + index + '"]').addClass('hover')
+          },
+          function () {
+            var index = $(this).data('index')
+            that.$leftFixedBody.find('tr[data-index="' + index + '"]').removeClass('hover')
+          }
+        )
+      this.$leftFixedBody
+        .find('tr[data-index]')
+        .off('hover')
+        .hover(
+          function () {
+            var index = $(this).data('index')
+            that.$body.find('tr[data-index="' + index + '"]').addClass('hover')
+          },
+          function () {
+            var index = $(this).data('index')
+            that.$body.find('> tr[data-index="' + index + '"]').removeClass('hover')
+          }
+        )
     }
     if (that.options.rightFixedColumns) {
       if (!this.$body.find('> tr[data-index]').length) {
@@ -374,12 +303,13 @@ $.BootstrapTable = class extends $.BootstrapTable {
         height -= top
       }
 
-      this.$rightFixedBody.css({
-        top: top,
-        width: this.$rightFixedWidth + 2,
-        height: height - 13
-      }).show()
-
+      this.$rightFixedBody
+        .css({
+          top: top,
+          width: this.$rightFixedWidth,
+          height: height - 14
+        })
+        .show()
 
       this.$body.find('> tr').each(function (i) {
         that.$rightFixedBody.find('tbody tr:eq(' + i + ')').height($(this).height())
@@ -389,56 +319,74 @@ $.BootstrapTable = class extends $.BootstrapTable {
       this.$tableBody.on('scroll', function () {
         that.$rightFixedBody.find('table').css('top', -$(this).scrollTop())
       })
-      this.$body.find('> tr[data-index]').off('hover').hover(function () {
-        var index = $(this).data('index')
-        that.$rightFixedBody.find('tr[data-index="' + index + '"]').addClass('hover')
-      }, function () {
-        var index = $(this).data('index')
-        that.$rightFixedBody.find('tr[data-index="' + index + '"]').removeClass('hover')
-      })
-      this.$rightFixedBody.find('tr[data-index]').off('hover').hover(function () {
-        var index = $(this).data('index')
-        that.$body.find('tr[data-index="' + index + '"]').addClass('hover')
-      }, function () {
-        var index = $(this).data('index')
-        that.$body.find('> tr[data-index="' + index + '"]').removeClass('hover')
-      })
+      this.$body
+        .find('> tr[data-index]')
+        .off('hover')
+        .hover(
+          function () {
+            var index = $(this).data('index')
+            that.$rightFixedBody.find('tr[data-index="' + index + '"]').addClass('hover')
+          },
+          function () {
+            var index = $(this).data('index')
+            that.$rightFixedBody.find('tr[data-index="' + index + '"]').removeClass('hover')
+          }
+        )
+      this.$rightFixedBody
+        .find('tr[data-index]')
+        .off('hover')
+        .hover(
+          function () {
+            var index = $(this).data('index')
+            that.$body.find('tr[data-index="' + index + '"]').addClass('hover')
+          },
+          function () {
+            var index = $(this).data('index')
+            that.$body.find('> tr[data-index="' + index + '"]').removeClass('hover')
+          }
+        )
 
-      console.log('this.$tableHeader', that.$tableHeader.find('thead th.bs-checkbox'))
-      console.log('this.$tableBody', this.$tableBody.find('tbody').find('.bs-checkbox'))
-
+      // console.log('this.$tableHeader', that.$tableHeader.find('thead th.bs-checkbox'))
+      // console.log('this.$tableBody', this.$tableBody.find('tbody').find('.bs-checkbox'))
 
       that.$el.off('check-all.bs.table').on('check-all.bs.table', function (e, rowsAfter, rowsBefore) {
-        console.log('check-all.bs.table', e, rowsAfter, rowsBefore)
+        // console.log('check-all.bs.table', e, rowsAfter, rowsBefore)
       })
 
-      that.$leftFixedHeader.find('thead').find('.bs-checkbox').off('change', 'input[type="checkbox"]').on('change', 'input[type="checkbox"]', function (e) {
-        var thisObj = $(this)
-        console.log('leftFixedHeader', thisObj.prop('checked'), that)
-        that.selectAllRow(thisObj.prop('checked'))
-        if (thisObj.prop('checked')) {
-          that.$el.bootstrapTable('checkAll')
-        } else {
-          that.$el.bootstrapTable('uncheckAll')
-        }
-      })
+      that.$leftFixedHeader
+        .find('thead')
+        .find('.bs-checkbox')
+        .off('change', 'input[type="checkbox"]')
+        .on('change', 'input[type="checkbox"]', function (e) {
+          var thisObj = $(this)
+          // console.log('leftFixedHeader', thisObj.prop('checked'), that)
+          that.selectAllRow(thisObj.prop('checked'))
+          if (thisObj.prop('checked')) {
+            that.$el.bootstrapTable('checkAll')
+          } else {
+            that.$el.bootstrapTable('uncheckAll')
+          }
+        })
 
-      that.$leftFixedBody.find('tbody').find('.bs-checkbox input[type="checkbox"]').off('change').on('change', function (e) {
-        var thisObj = $(e.target)
-        var index = thisObj.data('index')
-        console.log('leftFixedBody', thisObj.prop('checked'), index)
-        that.selectRow(index, thisObj.prop('checked'))
-        if (thisObj.prop('checked')) {
-          that.$el.bootstrapTable('check', index)
-        } else {
-          that.$el.bootstrapTable('uncheck', index)
-        }
-      })
-
+      that.$leftFixedBody
+        .find('tbody')
+        .find('.bs-checkbox input[type="checkbox"]')
+        .off('change')
+        .on('change', function (e) {
+          var thisObj = $(e.target)
+          var index = thisObj.data('index')
+          // console.log('leftFixedBody', thisObj.prop('checked'), index)
+          that.selectRow(index, thisObj.prop('checked'))
+          if (thisObj.prop('checked')) {
+            that.$el.bootstrapTable('check', index)
+          } else {
+            that.$el.bootstrapTable('uncheck', index)
+          }
+        })
     }
   }
 
-  selectRow (rowIndex, flag) {
+  selectRow(rowIndex, flag) {
     if (flag) {
       this.$leftFixedBody.find('tbody tr[data-index="' + rowIndex + '"]').addClass('selected')
       this.$rightFixedBody.find('tbody tr[data-index="' + rowIndex + '"]').addClass('selected')
@@ -448,8 +396,11 @@ $.BootstrapTable = class extends $.BootstrapTable {
     }
   }
 
-  selectAllRow (flag) {
-    this.$leftFixedBody.find('tbody tr').find('.bs-checkbox input[type="checkbox"]').prop('checked', flag)
+  selectAllRow(flag) {
+    this.$leftFixedBody
+      .find('tbody tr')
+      .find('.bs-checkbox input[type="checkbox"]')
+      .prop('checked', flag)
     if (flag) {
       this.$leftFixedBody.find('tbody tr[data-index]').addClass('selected')
       this.$rightFixedBody.find('tbody tr[data-index]').addClass('selected')
@@ -459,5 +410,3 @@ $.BootstrapTable = class extends $.BootstrapTable {
     }
   }
 }
-
-
